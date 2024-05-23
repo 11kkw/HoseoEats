7b9f227064ad053380b28fa6800bebecdae48c2e
11kkw <11kkw17@gmail.com>
2024-05-23 14:05:46 +0900

    Replace fetch with axios in geocode function

--- api/geocode.js
+++ api/geocode.js
@@ -1,10 +1,9 @@
-const fetch = require("node-fetch");
+const axios = require("axios");
 
 const geocode = async (req, res) => {
   const apiUrl = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode";
   const query = req.query.query || "default";
   const apiOptions = {
-    method: "GET",
     headers: {
       "X-NCP-APIGW-API-KEY-ID": process.env.NAVER_CLIENT_ID,
       "X-NCP-APIGW-API-KEY": process.env.NAVER_CLIENT_SECRET,
@@ -14,17 +13,12 @@ const geocode = async (req, res) => {
   try {
     console.log("Request URL:", `${apiUrl}?query=${encodeURIComponent(query)}`);
 
-    const response = await fetch(
+    const response = await axios.get(
       `${apiUrl}?query=${encodeURIComponent(query)}`,
       apiOptions
     );
 
-    if (!response.ok) {
-      throw new Error(`Error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    res.status(200).json(data);
+    res.status(200).json(response.data);
   } catch (error) {
     console.error("Geocode API error:", error.message);
     res.status(500).json({ error: "Failed to fetch data" });
