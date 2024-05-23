639bc3641756e006942f81eddb2e112cc909555a
11kkw <11kkw17@gmail.com>
2024-05-23 15:12:13 +0900

    testing

--- api/geocode.js
+++ api/geocode.js
@@ -9,13 +9,9 @@ const geocode = async (req, res) => {
       "X-NCP-APIGW-API-KEY": process.env.NAVER_CLIENT_SECRET,
     },
   };
-
   console.log("NAVER_CLIENT_ID:", process.env.NAVER_CLIENT_ID);
   console.log("NAVER_CLIENT_SECRET:", process.env.NAVER_CLIENT_SECRET);
-
   try {
-    console.log("Request URL:", `${apiUrl}?query=${encodeURIComponent(query)}`);
-
     const response = await axios.get(
       `${apiUrl}?query=${encodeURIComponent(query)}`,
       apiOptions
