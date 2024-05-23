1770df25df2f05c2b8035ca1a95f96282258bfdf
11kkw <11kkw17@gmail.com>
2024-05-23 16:26:31 +0900

    testing good

--- api/geocode.js
+++ api/geocode.js
@@ -4,11 +4,6 @@ const geocode = async (req, res) => {
   const apiUrl = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode";
   const query = req.query.query || "default";
 
-  // 환경 변수 및 디버깅 로그 출력
-  console.log("Testing environment variables");
-  console.log("NAVER_CLIENT_ID:", process.env.NAVER_CLIENT_ID);
-  console.log("NAVER_CLIENT_SECRET:", process.env.NAVER_CLIENT_SECRET);
-
   const apiOptions = {
     headers: {
       "X-NCP-APIGW-API-KEY-ID": process.env.NAVER_CLIENT_ID,
@@ -17,9 +12,6 @@ const geocode = async (req, res) => {
   };
 
   try {
-    console.log("Request URL:", `${apiUrl}?query=${encodeURIComponent(query)}`);
-    console.log("Request Headers:", apiOptions.headers);
-
     const response = await axios.get(
       `${apiUrl}?query=${encodeURIComponent(query)}`,
       apiOptions
